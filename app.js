0aede92e011dcdf4e4af9ff171cede0fb86f81de
Victor Tang <victortang1992@gmail.com>
2015-11-04 12:38:52 -0800

    buttons style minimal, start app

--- app.js
+++ app.js
@@ -2,17 +2,16 @@
 
 var app = angular.module('calculator',[]);
 
-app.factory('numbers', [function(){
-  var timers = {
-      cooldown: 5,
-      session: 25,
+app.factory('memory', [function(){
+  var storage = {
+      lastAns:1,
   };
-  return timers;
+  return storage;
 }]);//end of service
 
-app.controller('MainCtrl', ['$scope', 'numbers', '$interval', function($scope, numbers, $interval){
-    $scope.timers = time; // load service
-
+app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
+    $scope.storage = memory; // load service
+    
     
 }]);//end of controller
 
