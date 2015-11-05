3d5508cf68a3c32eb898bb89d6b07d74d0a36a25
Victor Tang <victortang1992@gmail.com>
2015-11-05 01:47:45 -0800

    wrapper functions work for empty, operator last situations include ^

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
         $scope.entered = "";
     };
     $scope.prefix = function(content){
-        var lastNum = /([()\w]+[-+/*]?)(?!.*\d)/g
+        var lastNum = /([()\w]+[-+/*^]?)(?!.*\d)/g
         var replacement = "(" + content + $scope.entered.match(lastNum) + ")";
         if ($scope.entered == ""){
             $scope.enter(content);
@@ -37,10 +37,21 @@ app.controller('MainCtrl', ['$scope', 'memory', function($scope, memory){
         }
     };
     $scope.wrapper = function(content){
-        var lastNum = /([()\w]+[-+/*]?)(?!.*\d)/g
+        var operators = /([-+/*^])/g
+        var lastNum = /([()\w]+[-+/*^]?)(?!.*\d)/g
         var replacement = content + "(" + $scope.entered.match(lastNum) + ")";
-        var altReplacement = content + "(" + $scope.entered.match(lastNum);
-
+        if ($scope.entered == ""){
+            $scope.entered = "0";
+            var altReplacement = content + "(" + $scope.entered.match(lastNum);
+            $scope.entered = $scope.entered.replace(lastNum,"");
+            $scope.entered = $scope.entered.concat(altReplacement);
+        }
+        else if($scope.entered.slice(-1).search(operators) != -1){
+            var altReplacement = content + "(" + $scope.entered.match(lastNum);
+            $scope.entered = $scope.entered.replace(lastNum,"");
+            $scope.entered = $scope.entered.concat(altReplacement);
+        }
+        else{
         $scope.entered = $scope.entered.replace(lastNum,"");
         $scope.entered = $scope.entered.concat(replacement);
         }
